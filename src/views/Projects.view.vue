0cc06310b8aa3e2155fbd5936896ee6fa5950d66
Eli <bernerielia@gmail.com>
2026-08-19 08:27:40 +0200

    fix: improve image handling in projects view

--- src/views/Projects.view.vue
+++ src/views/Projects.view.vue
@@ -25,7 +25,14 @@
             <h3 class="text-3xl font-bold p-4">Projects</h3>
             <div class="w-4xl border rounded-2xl p-4 border-neutral-700 hover:border-neutral-300 transition-all duration-300 hover:shadow-lg hover:scale-105" v-for="project in projects" :key="project.id">
                 <a :href="project.url" target="_blank" class="flex text-left gap-3">
-                    <img :src="project.image" class="h-18 w-18 object-cover rounded-lg" :alt="project.name">
+                    <img
+                        :src="project.image"
+                        class="h-18 w-18 object-cover rounded-lg"
+                        :class="{
+                            'brightness-0 dark:invert': project.image.toLowerCase().endsWith('.svg')
+                        }"
+                        :alt="project.name"
+                    >
                     <div class="flex flex-col">
                         <h6 class="text-xl">{{ project.name }}</h6>
                         <p class="text-sm">{{ project.description }}</p>
